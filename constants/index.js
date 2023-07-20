c0b775f9c8d2a1472de1ea924b9bf37d9a594e29
Rebwar Hosein Poori <rebwar.poori@gmail.com>
2023-07-20 11:39:15 +0330

    Update index.js
    add my contract

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x109af36fae6143c4a8bcd25211209048c2ca62d5";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
